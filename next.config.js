1dbf370ac8f30835e9e04f17331d010a537cfb43
Ben Binder <ben.binder11@gmail.com>
2023-11-13 10:58:15 -0600

    Update allowed origins in config.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'https://pocketbook.binderlab.io',
+        'http://localhost:3000',
+        'http://0.0.0.0:3000'
+      ]
+    }
+  }
 }
 
 module.exports = nextConfig
